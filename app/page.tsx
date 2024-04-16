0164ff199df5ffc0416bc1ec5e92cf48322aabf0
andrezandonai <116611143+andrezandonai@users.noreply.github.com>
2024-04-16 20:04:42 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1 className="text-center ">PAGINA INICIAL 2</h1> 
+   <h1 className="text-center ">ESCOLA TECNOLOGICA </h1> 
   );
 }
